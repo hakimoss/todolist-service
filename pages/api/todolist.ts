d2d6515412bab4974a029f50cc63bfe9020adf36
hakimoss <hakimos@live.ca>
2023-02-05 14:12:18 -0500

    demande un object avec la key element

--- pages/api/todolist.ts
+++ pages/api/todolist.ts
@@ -2,28 +2,31 @@ import { getListDao } from '@/dao/todoDao';
 import { addElementToList, deleteElementToList } from '@/services/todoService';
 import { BadRequestException, Body, createHandler, Delete, Get, HttpCode, Post, ValidationPipe } from 'next-api-decorators';
 
+type ListElement = {
+  element: string;
+}
+
 class ToDoListHandler {
   @Get()
   getToDoList() {
     return getListDao();
   }
   @Post()
-  createToDoList(@Body(ValidationPipe()) listElement: string[]) {
-    console.log('typeof listElement', Array.isArray(listElement));
-    addElementToList(listElement[0])
-    if(!Array.isArray(listElement)) {
-        throw new BadRequestException("wrong type of data, need an array");
+  createToDoList(@Body(ValidationPipe()) listElement: ListElement) {
+    addElementToList(listElement.element)
+    if(typeof listElement === "string") {
+        throw new BadRequestException("wrong type of data, need an string");
     }
     
-    return `You just add [${listElement[0]}] to todoList.`;
+    return `You just add [${listElement.element}] to todoList.`;
   }
   @Delete()
-  deleteToDoList(@Body(ValidationPipe()) listElement: Array<string>) {
-    if(!Array.isArray(listElement)) {
-        throw new BadRequestException("wrong type of data, need an array");
+  deleteToDoList(@Body(ValidationPipe()) listElement: ListElement) {
+    if(typeof listElement === "string") {
+      throw new BadRequestException("wrong type of data, need an string");
     }
-    deleteElementToList(listElement[0])
-    return `You just delete [${listElement[0]}] from todoList.`;
+    deleteElementToList(listElement.element)
+    return `You just delete [${listElement.element}] from todoList.`;
   }
 }
 
